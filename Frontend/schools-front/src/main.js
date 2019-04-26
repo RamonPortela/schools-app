6691ea439f2c3f2eec5fa64ea843d30431ae3fab
Ramon Portela Santos <portela.ramon96@gmail.com>
2019-04-26 01:11:30 -0300

    Criação da home

--- Frontend/schools-front/src/main.js
+++ Frontend/schools-front/src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
+import VueMaterial from "vue-material";
 import App from "./App.vue";
 import router from "./router";
+import "vue-material/dist/vue-material.min.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMaterial);
+Vue.component("router-link", Vue.options.components.RouterLink);
+Vue.component("router-view", Vue.options.components.RouterView);
 
 new Vue({
   router,
--- Frontend/schools-front/src/views/Home.vue
+++ Frontend/schools-front/src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <md-card md-with-hover @click.native="goToSchools">
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Escolas</div>
+          <div class="md-subhead">Quantidade de escolas cadastradas</div>
+        </md-card-header>
+
+        <md-card-content>
+          <AnimatedCounter :value="schoolsQuantity" :cssClass="{'md-display-4': true}"/>
+        </md-card-content>
+      </md-ripple>
+    </md-card>
+    <md-card md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Turmas</div>
+          <div class="md-subhead">Quantidade de turmas cadastradas</div>
+        </md-card-header>
+
+        <md-card-content>
+          <AnimatedCounter :value="classesQuantity" :cssClass="{'md-display-4': true}"/>
+        </md-card-content>
+      </md-ripple>
+    </md-card>  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AnimatedCounter from "@/components/AnimatedCounter.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      schoolsQuantity: 0,
+      classesQuantity: 0,
+    }
+  },
   components: {
-    HelloWorld
+    AnimatedCounter
+  },
+  mounted(){
+    fetch('http://localhost:64552/api/school/count')
+      .then(response => response.text())
+      .then(data => this.schoolsQuantity = parseInt(data))
+      .catch(e => console.error(e));
+  },
+  methods:{
+    goToSchools(){
+      this.$router.push('/escolas');
+    }
   }
-};
+}
 </script>
+
+<style lang="scss" scoped>
+  .home{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .md-card {
+      width: 320px;
+      margin: 4px;
+      display: inline-block;
+      vertical-align: top;
+
+      .md-card-content{
+        text-align: center;
+
+        .md-display-4{
+          color: var(--md-theme-default-primary, #69f0ae);
+        }
+      }
+    }
+  }
+</style>
